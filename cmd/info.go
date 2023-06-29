cb7d00d1cc84ecc5824518d54aba1db8acb31a00
Sumit Kumar Prasad <prasad28sumit@gmail.com>
2023-06-29 21:49:49 +0530

    Add user-friendly print message in info

--- cmd/info.go
+++ cmd/info.go
@@ -13,13 +13,8 @@ import (
 // infoCmd represents the info command
 var infoCmd = &cobra.Command{
 	Use:   "info",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Prints the location of data files",
+	Long:  `This command is used to know about the location of the genereted data stored on the machine`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(user.Info())
 	},
--- makefile
+++ makefile
@@ -23,21 +23,25 @@ reset:
 	pass-manager reset secret
 
 build-win-amd64:
+	set CGO_ENABLED=0 
 	set GOOS=windows
 	set GOARCH=amd64
 	go build -o bin/passm-win-amd64.exe
 
 build-linux-amd64:
+	set CGO_ENABLED=0
 	set GOOS=linux
 	set GOARCH=amd64
 	go build -o bin/passm-linux-amd64.exe
 
 build-win-arm:
+	set CGO_ENABLED=0 
 	set GOOS=windows
 	set GOARCH=arm
 	go build -o bin/passm-win-arm.exe
 
 build-linux-arm:
+	set CGO_ENABLED=0 
 	set GOOS=linux
 	set GOARCH=arm
 	go build -o bin/passm-linux-arm.exe
